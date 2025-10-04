b132e7141200898834a8150b3edffcbed02411fd
l429609201 <429609201@qq.com>
2025-10-04 16:32:28 +0800

    modified:   data-center/src/config.py

--- data-center/src/config.py
+++ data-center/src/config.py
@@ -3,7 +3,8 @@
 """
 import os
 from typing import List, Optional
-from pydantic import BaseSettings, validator
+from pydantic_settings import BaseSettings
+from pydantic import validator
 
 class Settings(BaseSettings):
     """应用配置"""
